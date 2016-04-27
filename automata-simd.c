6e3ed870d7e06af8d7c2b4331412b1146b580b54
Joseph Connor <josephc346@gmail.com>
2016-04-27 17:05:03 -0400

    fix potential alignment issue, add more comments

--- automata-simd.c
+++ automata-simd.c
@@ -4,6 +4,8 @@
 
 // width must be multiple of 16!!!
 void do_ca(uint8_t *grid, uint8_t *rules, int width, int ngens) {
+    /* we're going to sentinalize the front and back of the list to simulate
+       continuous boundary conditions, so add space for 2 more items */
     int padded_width = width + 2;
 
     int geni, off, gen_end;
@@ -11,7 +13,7 @@ void do_ca(uint8_t *grid, uint8_t *rules, int width, int ngens) {
     uint8_t *newgen;
 
     __m128i *new, *old, *end;
-    __m128i table = *((__m128i*)rules);
+    __m128i table = _mm_loadu_si128((__m128i*)rules);
     __m128i tmp;
 
     gen_end = ngens-1;
@@ -22,7 +24,7 @@ void do_ca(uint8_t *grid, uint8_t *rules, int width, int ngens) {
         gen[-1] = gen[width-1];
         gen[width] = gen[0];
 
-        // add neighborhoods
+        // add neighborhoods, radius 1
         end = (__m128i*)(newgen + width);
         for (off=-1; off <= 1; ++off) {
             old = (__m128i*)(gen + off);
